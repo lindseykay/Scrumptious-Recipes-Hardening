bb068ca4de923b0e61046c8dc1df7cb10bd3ee53
Lindsey <lindseykaycarlson@outlook.com>
2022-08-30 18:06:58 -0700

    log in and logout now work

--- scrumptious/urls.py
+++ scrumptious/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path, reverse_lazy
 from django.views.generic.base import RedirectView
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -24,6 +26,7 @@ urlpatterns = [
     path(
         "",
         RedirectView.as_view(url=reverse_lazy("recipes_list")),
-        name="home",
-    ),
+        name="home"),
+    path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
+    path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
