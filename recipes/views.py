14ecb868d1370cbc7105fec284570c8f9fe3d585
Lindsey <lindseykaycarlson@outlook.com>
2022-09-02 18:46:24 -0700

    add ingredient button disappears when item added to cart

--- recipes/views.py
+++ recipes/views.py
@@ -79,6 +79,10 @@ class RecipeDetailView(LoginRequiredMixin, DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["rating_form"] = RatingForm()
+        foods = []
+        for item in self.request.user.shopping_items.all():
+            foods.append(item.food_item)
+        context["food_in_shopping_list"] = foods
         return context
 
 
